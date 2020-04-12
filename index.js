1d1e1cfdcdba2003366b2e1047650728469a8471
Andrew Mueller <iglooworks1505@gmail.com>
2020-04-11 19:23:11 -0500

    installed materialize a different way and prayed a little

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-M.AutoInit();
+const rootDir = "https://api.openbrewerydb.org";
+const perPage = "&per_page=5";
+const pageOffset = 1;
+
+// document.getElementById("idOfNextPageButton").addEventListener("click", () => {
+// e.preventDefault()
+//   pageCount++;
+// });
+// document.getElementById("idOfLastPageButton").addEventListener("click", (e) => {
+// e.preventDefault()
+//   pageCount --;
+// });
+const byCity = async (city) => {
+  try {
+    const res = await axios.get(
+      `${rootDir}/breweries?by_city=${city}${perPage}`
+    );
+    console.log("byCity returns", res.data);
+    // sendToZach(res.data)
+  } catch (error) {
+    console.log(error);
+  }
+};
+// byCity("Milwaukee");
+
+const byZip = async (zipCode) => {
+  try {
+    const res = await axios.get(
+      `${rootDir}/breweries?by_postal=${zipCode}${perPage}&page=${pageOffset}`
+    );
+    console.log("byZip returns", res.data);
+    // sendToZach(res.data)
+  } catch (error) {
+    console.log(error);
+  }
+};
+// byZip(53202);
+
+const cityState = async (city, state) => {
+  try {
+    const res = await axios.get(
+      `${rootDir}/breweries?by_city=${city}&by_state=${state}${perPage}&page=${pageOffset}`
+    );
+    console.log("cityState returns", res.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+cityState("Milwaukee", "Wisconsin");
