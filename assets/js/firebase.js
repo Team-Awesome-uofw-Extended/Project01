ad8f344fd0d9e84f6b9fdfb4ec33203f80be05ae
Tyler Blakeman <tylerblakeman@gmail.com>
2020-04-13 17:14:18 -0500

    added firebase api key and linked uuiv4 npm

---
+++ assets/js/firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC5xOrnUb8u8nAyD0gQvyDylQWsMQOo0Gk",
+    authDomain: "brewcrawler-64bc1.firebaseapp.com",
+    databaseURL: "https://brewcrawler-64bc1.firebaseio.com",
+    projectId: "brewcrawler-64bc1",
+    storageBucket: "brewcrawler-64bc1.appspot.com",
+    messagingSenderId: "982432305908",
+    appId: "1:982432305908:web:40dceddedaaed9f83644a1",
+    measurementId: "G-66M9T4ETQS"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  console.log(uuid())
