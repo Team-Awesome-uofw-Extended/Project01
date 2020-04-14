afd6aa8e044f2fa795066051ba361664ad4034dd
Tyler Blakeman <tylerblakeman@gmail.com>
2020-04-13 20:24:15 -0500

    finally something showing up in firebase

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,30 +1,33 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyC5xOrnUb8u8nAyD0gQvyDylQWsMQOo0Gk",
-    authDomain: "brewcrawler-64bc1.firebaseapp.com",
-    databaseURL: "https://brewcrawler-64bc1.firebaseio.com",
-    projectId: "brewcrawler-64bc1",
-    storageBucket: "brewcrawler-64bc1.appspot.com",
-    messagingSenderId: "982432305908",
-    appId: "1:982432305908:web:40dceddedaaed9f83644a1",
-    measurementId: "G-66M9T4ETQS"
+    apiKey: "AIzaSyBjS1MGlsOhRi0LHvUCLtoP8yzv46G-YPg",
+    authDomain: "brewcrawler-ae0b1.firebaseapp.com",
+    databaseURL: "https://brewcrawler-ae0b1.firebaseio.com",
+    projectId: "brewcrawler-ae0b1",
+    storageBucket: "brewcrawler-ae0b1.appspot.com",
+    messagingSenderId: "1058211844569",
+    appId: "1:1058211844569:web:03243ecfc694b7bc3989c8"
   };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
-var crawlCode
-var 
+var database = firebase.database();
+
+
+var crawlCode = 0;
+var passingCrawl;
 
 $(".crawl-submit").click(function() {
     crawlCode = Math.round(Math.random() * 1000000);
-    window.sessionStorage.getItem(crawlArray);
-    console.log("success?")
+    console.log("Crawlcode" + crawlCode);
+    console.log(passingCrawl);
+    console.log("success?");
     writeCrawl();
 })
 
 
-function writeCrawl(crawlCode, crawlArray) {
-    firebase.database().ref(`crawl-code/` + crawlCode).set({
-        stops: crawlArray;
-    })
+function writeCrawl() {
+    firebase.database().ref(`crawl-code/${crawlCode}`).set({
+        stops: "hi"
+    });
 }
